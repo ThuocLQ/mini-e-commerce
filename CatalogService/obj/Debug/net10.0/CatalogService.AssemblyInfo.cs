b1649c15ea37b8f8c871739803510f903adff6d4
ThuocLQ <thuoclq01@gmail.com>
2026-05-01 21:29:21 +0700

    build

--- CatalogService/obj/Debug/net10.0/CatalogService.AssemblyInfo.cs
+++ CatalogService/obj/Debug/net10.0/CatalogService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatalogService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1f4edb4f6dde3eae714f02cf108d2277bde6ec6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4706da956b58c46982a6689ad5cc0533ec88a45")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatalogService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatalogService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
